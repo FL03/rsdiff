043a5f4d67f3f4138d16596aef8a766e894c0287
Joe McCain III <jo3mccain@icloud.com>
2024-04-08 12:04:41 -0500

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- macros/tests/autodiff.rs
+++ macros/tests/autodiff.rs
@@ -122,8 +122,8 @@ fn test_pow() {
     let (x, y) = (2f64, 3f64);
     let y_i: i32 = 3;
     assert_eq!(autodiff!(x: x.pow(y)), y * x.pow(2));
-    // assert_eq!(autodiff!(y: x.pow(y)), x.pow(y) * y.ln());
-    assert_eq!(autodiff!(x: 2f64.powf(y)), 0f64);
+    assert_eq!(autodiff!(y: x.pow(y)), x.pow(y) * y.ln());
+    assert_eq!(autodiff!(x: 2f64.pow(y)), 0f64);
     assert_eq!(autodiff!(y: 2f64.pow(y)), 2f64.pow(y) * y.ln());
     assert_eq!(autodiff!(x: x.powi(y)), y * x.pow(2));
 }
